444ac3aa374466dd1a8a0d4e56e17c45e094fb9d
caowenlong <>
2018-11-08 10:03:40 +0800

    first

---
+++ src/main/java/com/wn/service/App.java
@@ -0,0 +1,19 @@
+package com.wn.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        List<String> list = new ArrayList<>();
+
+    }
+}
---
+++ src/main/java/com/wn/service/collection/StudyHashMap.java
@@ -0,0 +1,27 @@
+package com.wn.service.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 学习hashMap的原理和源码
+ * @author: cwenlong
+ * @date: 2018-11-07 16:26
+ *
+ * 问题：
+ * 1.hashMap的存、取规则？
+ * 2.hashMap的扩容规则？
+ * 3.hashMap初始的容量大小应该如何选择？
+ */
+public class StudyHashMap {
+
+    public static void main(String[] args) {
+
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put(null, "key=null");
+
+
+
+
+    }
+}
